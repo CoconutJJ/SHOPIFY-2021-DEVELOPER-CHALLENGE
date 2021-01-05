712e79d1867b21dc7ee9e8a48ba092c8021d0acb
David Yue <davidyue5819@gmail.com>
2021-01-05 15:00:35 -0800

    Added delete functionality

--- js/index.js
+++ js/index.js
@@ -9,8 +9,24 @@ function uploadFile() {
 
     let reqData = new FormData()
 
+    let allowedExtensions = ["PNG", "JPG", "GIF", "JPEG"]
+
+    if (images.files.length == 0) {
+        alert('No Image')
+        return;
+    }
 
     for (let i = 0; i < images.files.length; i++) {
+
+        let filename = images.files[i].name
+
+        let ext = filename.split(".").pop()
+
+        if (allowedExtensions.indexOf(ext.toUpperCase()) == -1) {
+            alert("You must upload a PNG, JPG or GIF image!")
+            return
+        }
+
         reqData.append("file", images.files[i])
     }
 
@@ -19,25 +35,60 @@ function uploadFile() {
     xhttp.onreadystatechange = () => {
 
         if (xhttp.readyState != 4) return;
-
-        alert(xhttp.responseText);
-
+        
+        response = JSON.parse(xhttp.responseText)
+        
+        if (!response['state']) {
+            alert(response['msg']);
+        } else {
+            displayImages();
+        }
     }
 
-    xhttp.open("POST","/add");
+    xhttp.open("POST", "/add");
     xhttp.send(reqData);
+
+}
+
+/**
+ * 
+ * @param {string} imageName 
+ * @param {HTMLImageElement} imgElement 
+ * @param {HTMLInputElement} delBtn
+ */
+function deleteImage(imageName, imgElement) {
+
+    let xhttp = new XMLHttpRequest()
+
+
+    xhttp.onreadystatechange = () => {
+
+        if (xhttp.readyState != 4) return;
+
+        response = JSON.parse(xhttp.responseText)
+
+        if (response['state']) {
+            imgElement.parentElement.removeChild(imgElement);
+        } else {
+            alert(response['msg']);
+        }
     
+    }
+
+    xhttp.open("DELETE", "/images/" + imageName)
+    xhttp.send();
+
 }
 
 function displayImages() {
 
     let display = document.getElementById('images');
-
+    display.innerHTML = ""
     let xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState != 4) return;
-        
+
         imagesfiles = JSON.parse(xhttp.responseText);
 
         for (let f of imagesfiles) {
@@ -45,22 +96,27 @@ function displayImages() {
             let img = new Image()
 
             img.src = "/view/" + f
-            
-            display.appendChild(img);
 
+            img.addEventListener("click", () => {
+                if (confirm("Delete this image?")) {
+                    deleteImage(f, img);
+                }
+                
+            })
+
+            display.appendChild(img);
         }
 
     }
     xhttp.open('GET', "/images")
     xhttp.send();
-
 }
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    document.getElementById("upload").addEventListener("click", ()=> {
+    document.getElementById("upload").addEventListener("click", () => {
 
         uploadFile();
 
--- server.py
+++ server.py
@@ -1,30 +1,45 @@
 from flask import Flask, json, request
 from flask.helpers import send_from_directory
 import os
+import uuid
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-
     return send_from_directory(".", "index.html")
+
 @app.route("/js/<path:path>", methods=["GET"])
 def js(path):
     return send_from_directory("js", path);
 
-@app.route("/images", methods=['GET'])
-def images():
-    image_files = os.listdir("./images")
+@app.route('/images', methods=['GET'], defaults={"name": None})
+@app.route("/images/<name>", methods=['DELETE'])
+def images(name):
 
+    if request.method == 'DELETE':
+        images = os.listdir("images")
 
-    return json.dumps(image_files)
+        if name in images:
+            os.remove(os.path.join("images/", name))
+
+            return json.dumps({
+                "state": True,
+                "msg": "Image deleted"
+            })
+        return json.dumps({
+            "state": False,
+            "msg": "Image does not exist"
+        })
+
+    else:
+        
+        image_files = os.listdir("./images")
+        return json.dumps(image_files)
+
 @app.route("/view/<path:path>", methods=['GET'])
 def view(path):
-
     return send_from_directory("./images", path)
     
-
-
-
 @app.route("/add", methods=['POST'])
 def addImage():
 
@@ -35,6 +50,7 @@ def addImage():
     if 'file' not in request.files:
 
         return json.dumps({
+            "state": False,
             "msg": "No image"
         })
     
@@ -42,12 +58,16 @@ def addImage():
 
     if len(file.filename) == 0:
         return json.dumps({
+            "state": False,
             "msg": "No image"
         })
 
-    file.save(os.path.join("images", file.filename))
+    fname, ext = file.filename.split(".")
+
+    file.save(os.path.join("images", fname + uuid.uuid4().hex + "." + ext))
 
     return json.dumps({
+        "state": True,
         "msg": "Uploaded"
     })
 
